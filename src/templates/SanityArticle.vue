6cb4a067e734b9742fda8c72e2c366f5a0fe3d21
Lukas <lukas@youtuberis.lt>
2020-06-07 18:37:30 +0300

    Style article page

--- src/templates/SanityArticle.vue
+++ src/templates/SanityArticle.vue
@@ -1,17 +1,54 @@
 <template>
-    <Layout>
-        <article>
-            <header>{{ $page.article.title }}</header>
+    <Layout containerClass="flex flex-col items-center">
+        <g-image
+            v-if="hero"
+            :src="imageUrl(hero).height(400).url()"
+            :alt="hero.description"
+            class="w-full pb-4"
+        />
+        <article class="w-full sm:w-3/4 lg:w-1/2">
+            <header class="pb-8 text-left">
+                <h1 class="text-5xl font-light">
+                    {{ post.title }}
+                </h1>
 
-            <block-content :blocks="$page.article._rawBody"></block-content>
+                <div class="flex justify-between text-sm tracking-wide">
+                    <p class="text-gray-800 py-2 md:py-0">
+                        <time
+                            :datatime="post._updatedAt"
+                            :title="
+                                'Atnaujinta ' +
+                                new Date(post._updatedAt).toLocaleString('lt')
+                            "
+                        >
+                            {{
+                                new Date(post._updatedAt).toLocaleDateString(
+                                    'lt'
+                                )
+                            }}
+                        </time>
+                    </p>
 
-            <footer>
-                Kategorijos:
-                <ul>
-                    <li v-for="cat in $page.article.categories" :key="cat.id">
-                        {{ cat.title }}
-                    </li>
-                </ul>
+                    <p title="Kategorijos">
+                        {{ post.categories.map(c => c.title).join(', ') }}
+                    </p>
+                </div>
+            </header>
+
+            <main class="text-justify pb-8">
+                <block-content :blocks="post._rawBody"></block-content>
+            </main>
+
+            <footer class="py-4 text-sm tracking-wide">
+                <p>
+                    Pirmą kartą paskelbta
+                    <time
+                        :datatime="post._createdAt"
+                        :title="new Date(post._createdAt).toLocaleString('lt')"
+                    >
+                        {{ new Date(post._createdAt).toLocaleDateString('lt') }}
+                    </time>
+                </p>
             </footer>
         </article>
     </Layout>
@@ -19,12 +56,25 @@
 
 <script>
     import BlockContent from '@/components/BlockContent';
+    import imageUrl from '@/helpers/sanityImage';
+
     export default {
         name: 'Article',
         metaInfo() {
             return {
-                title: this.$page.article.title,
+                title: this.post.title,
             };
+        },
+        computed: {
+            post() {
+                return this.$page.post;
+            },
+            hero() {
+                return this.post.mainImage;
+            },
+        },
+        methods: {
+            imageUrl,
         },
         components: {
             BlockContent,
@@ -34,17 +84,23 @@
 
 <page-query>
     query Article($id: ID!) {
-        # metadata {
-        #   sanityOptions {
-        #     projectId
-        #     dataset
-        #   }
-        # }
-        article: sanityArticle(id: $id) {
+        post: sanityArticle(id: $id) {
             title
-            _createdAt(format: "MM, YYYY")
+            _createdAt
+            _updatedAt
+            mainImage {
+                asset {
+                    url
+                    description
+                }
+                crop {
+                    top
+                    bottom
+                    left
+                    right
+                }
+            }
             categories {
-                id
                 title
             }
             _rawBody
