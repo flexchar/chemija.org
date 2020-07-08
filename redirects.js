a28ba9d6bd41ad2da8196b8887aa47a28b22ded5
Lukas Vanagas <lukas@youtuberis.lt>
2020-07-08 14:26:36 +0300

    Add redirects for two upgraded pages

--- redirects.js
+++ redirects.js
@@ -1,5 +1,15 @@
 module.exports = [
     {
+        from: '/cheminis-skaiciuotuvas.html',
+        to: '/cheminis-skaiciuotuvas',
+        code: 301,
+    },
+    {
+        from: '/egzaminai.html',
+        to: '/egzaminai',
+        code: 301,
+    },
+    {
         from: '/*',
         to: 'https://chemija-legacy.netlify.app/:splat',
         code: 200,
